f11f5b690d72aac90d46ec5a6c329d0145758496
suraid121 <tonoyofficial121@gmail.com>
2025-07-30 22:19:00 +0600

    db created

---
+++ src/db/restaurantsCardData.js
@@ -0,0 +1,203 @@
+const restaurantsCardData = [
+
+
+    {
+        name: 'Lokma',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 5,
+        silverStar: 0,
+        ratings: 5.0,
+        reviews: 816,
+        img: [
+            '/lokma-1.svg',
+            '/lokma-2.svg',
+            '/iori-1.svg',
+            '/kitoko-1.svg',
+        ],
+        location: 'Paris'
+    },
+,
+
+    {
+        name: 'Bella Italia',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 5,
+        silverStar: 0,
+        ratings: 5.0,
+        reviews: 875,
+        img: [
+            '/bella-italia-1.svg',
+            '/bottega-1.svg',
+            '/bottega-2.svg',
+            '/bottega-3.svg',
+        ],
+        location: 'Singapour'
+    },
+    {
+        name: 'Marafuku Ramen',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 3,
+        silverStar: 2,
+        ratings: 3.5,
+        reviews: 87,
+        img: [
+            '/marafuku-ramen-1.svg',
+            '/ngalley-1.svg',
+            '/starbelly-1.svg',
+            '/the-melt-1.svg',
+        ],
+        location: 'Paris'
+    },
+    {
+        name: 'The snug',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 4,
+        silverStar: 0,
+        ratings: 4.5,
+        reviews: 65,
+        img: [
+            '/the-snug-1.svg',
+            '/bottega-1.svg',
+            '/bottega-2.svg',
+            '/bottega-3.svg',
+        ],
+        location: 'Paris'
+    },
+    {
+        name: 'Starbelly',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 4,
+        silverStar: 1,
+        ratings: 4.25,
+        reviews: 54,
+        img: [
+            '/starbelly-1.svg',
+            '/bottega-4.svg',
+            '/diogonal-1.svg',
+            '/iori-1.svg',
+        ],
+        location: 'Paris'
+    },
+        {
+        name: 'Arabia Nights',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 5,
+        silverStar: 0,
+        ratings: 5.0,
+        reviews: 776,
+        img: [
+            '/arabia-nights-1.svg',
+            '/arabia-nights-2.svg',
+            '/little-shucker-1.svg',
+            '/little-shucker-2.svg',
+        ],
+        location: 'Paris'
+    },
+    {
+        name: 'Iori',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 3,
+        silverStar: 1,
+        ratings: 3.75,
+        reviews: 24,
+        img: [
+            '/iori-1.svg',
+            '/kitoko-1.svg',
+            '/lokma-1.svg',
+            '/lokma-2.svg',
+        ],
+        location: 'Paris'
+    },
+    {
+        name: 'Ngalley',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 3,
+        silverStar: 2,
+        ratings: 3.5,
+        reviews: 124,
+        img: [
+            '/ngalley-1.svg',
+            '/little-shucker-1.svg',
+            '/little-shucker-2.svg',
+            '/bella-italia-1.svg',
+        ],
+        location: 'Singapour'
+    },
+    {
+        name: 'diogonal',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 4,
+        silverStar: 0,
+        ratings: 4.5,
+        reviews: 24,
+        img: [
+            '/diogonal-1.svg',
+            '/kitoko-1.svg',
+            '/kitoko-1.svg',
+            '/lokma-1.svg',
+        ],
+        location: 'Singapour'
+    },
+    {
+        name: 'Kitoko',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 2,
+        silverStar: 2,
+        ratings: 3,
+        reviews: 24,
+        img: [
+            '/kitoko-1.svg',
+            '/lokma-1.svg',
+            '/lokma-2.svg',
+            '/diogonal-1.svg',
+        ],
+        location: 'Singapour'
+    },
+    {
+        name: 'The melt',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 4,
+        silverStar: 0,
+        ratings: 4.5,
+        reviews: 20,
+        img: [
+            '/the-melt-1.svg',
+            '/ngalley-1.svg',
+            '/iori-1.svg',
+            '/kitoko-1.svg',
+        ],
+        location: 'Singapour'
+    },
+        {
+        name: 'Little Shucker',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 5,
+        silverStar: 0,
+        ratings: 5.0,
+        reviews: 826,
+        img: [
+            '/little-shucker-1.svg',
+            '/little-shucker-2.svg',
+            '/bottega-4.svg',
+            '/diogonal-1.svg',
+        ],
+        location: 'Singapour'
+    },
+        {
+        name: 'Bottega',
+        des: 'The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.',
+        goldStar: 5,
+        silverStar: 0,
+        ratings: 5.0,
+        reviews: 872,
+        img: [
+            '/bottega-1.svg',
+            '/bottega-2.svg',
+            '/bottega-3.svg',
+            '/bottega-4.svg',
+        ],
+        location: 'Singapour'
+    }
+];
+
+export default restaurantsCardData;
